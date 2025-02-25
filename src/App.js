da66e53b05a110371d80d992a3ad571c63acfe58
saitgurdag <sait.gurdag@gmail.com>
2025-02-25 13:58:53 +0300

    refactor: improve code formatting and add quantity state management in ProductCard

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import React from 'react';
-import './App.css';
-import products from '../common/dummy';
-import { Box, Button, Typography, TextField } from '@mui/material';
+import React, { useState } from "react";
+import "./App.css";
+import products from "../common/dummy";
+import { Box, Button, Typography, TextField } from "@mui/material";
+import { useStore } from "shell/store";
 
 const cardStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  width: '200px',
-  padding: '16px',
-  borderRadius: '12px',
-  boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
-  backgroundColor: '#fff',
-  transition: 'transform 0.3s ease-in-out',
-  '&:hover': {
-    transform: 'scale(1.05)',
-    boxShadow: '0px 6px 18px rgba(0, 0, 0, 0.2)',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "space-between",
+  width: "200px",
+  padding: "16px",
+  borderRadius: "12px",
+  boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+  backgroundColor: "#fff",
+  transition: "transform 0.3s ease-in-out",
+  "&:hover": {
+    transform: "scale(1.05)",
+    boxShadow: "0px 6px 18px rgba(0, 0, 0, 0.2)",
   },
 };
 
 const listStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)', // 4 sütunlu grid
-  gap: '48px', // Elemanlar arası boşluk
-  padding: '24px',
-  justifyContent: 'center',
-  '@media (max-width: 1200px)': {
-    gridTemplateColumns: 'repeat(3, 1fr)', // Küçük ekranlarda 2 sütun
+  display: "grid",
+  gridTemplateColumns: "repeat(4, 1fr)", // 4 sütunlu grid
+  gap: "48px", // Elemanlar arası boşluk
+  padding: "24px",
+  justifyContent: "center",
+  "@media (max-width: 1200px)": {
+    gridTemplateColumns: "repeat(3, 1fr)", // Küçük ekranlarda 2 sütun
   },
-  '@media (max-width: 900px)': {
-    gridTemplateColumns: 'repeat(2, 1fr)', // Küçük ekranlarda 2 sütun
+  "@media (max-width: 900px)": {
+    gridTemplateColumns: "repeat(2, 1fr)", // Küçük ekranlarda 2 sütun
   },
-  '@media (max-width: 600px)': {
-    gridTemplateColumns: 'repeat(1, 1fr)', // Mobilde 1 sütun
+  "@media (max-width: 600px)": {
+    gridTemplateColumns: "repeat(1, 1fr)", // Mobilde 1 sütun
   },
 };
 
-const ProductCard = product => {
+const ProductCard = (product) => {
+  const { addToCart } = useStore();
+  const [quantity, setQuantity] = useState(1);
+
+  const handleClick = (item, quantity) => {
+    console.log("item", { ...item, quantity: quantity });
+    addToCart({ ...item, quantity: quantity });
+  };
+
   return (
     <Box sx={cardStyle}>
       <Typography variant="body1" fontWeight="bold">
@@ -49,10 +58,15 @@ const ProductCard = product => {
       <TextField
         type="number"
         size="small"
+        onChange={(e) => setQuantity(e.target.value)}
         defaultValue={1}
-        sx={{ width: '80px', margin: '8px 0' }}
+        sx={{ width: "80px", margin: "8px 0" }}
       />
-      <Button variant="contained" color="secondary">
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => handleClick(product.product, quantity)}
+      >
         Sepete Ekle
       </Button>
     </Box>
@@ -66,7 +80,7 @@ const App = () => {
         This is your products micro.
       </Typography>
       <Box className="list" sx={listStyle}>
-        {products.map(item => {
+        {products.map((item) => {
           return <ProductCard product={item} />;
         })}
       </Box>
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
-const deps = require('./package.json').dependencies;
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
     compress: true,
     port: 3001,
     hot: true,
     historyApiFallback: true,
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
@@ -23,47 +23,48 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name: 'product',
-      filename: 'remoteEntry.js',
+      name: "product",
+      filename: "remoteEntry.js",
       // kendini expose edicek: key= diger uygulamalarin bu componente hangi isimle erisebilecegi
       // bir uygulamanin tum sayfasini veya belli bir componentini expose edebilirsin
       exposes: {
-        './App': './src/App',
+        "./App": "./src/App",
       },
+      remotes: { shell: "shell@http://localhost:3000/remoteEntry.js" },
       shared: {
         ...deps,
         react: {
           singleton: true, // bu sayede tum uygulamalar tek bir react instance i uzerinden calisir
           requiredVersion: deps.react,
         },
-        'react-dom': {
+        "react-dom": {
           singleton: true,
-          requiredVersion: deps['react-dom'],
+          requiredVersion: deps["react-dom"],
         },
-        '@mui/material': {
+        "@mui/material": {
           singleton: true,
-          requiredVersion: deps['@mui/material'],
+          requiredVersion: deps["@mui/material"],
         },
-        '@mui/icons-material': {
+        "@mui/icons-material": {
           singleton: true,
-          requiredVersion: deps['@mui/icons-material'],
+          requiredVersion: deps["@mui/icons-material"],
         },
       },
     }),
